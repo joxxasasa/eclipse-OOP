f316cf1d0f9c9ca4f8e1e24f827bca8106aa2d8e
joxxasasa <joxxasasa@gmail.com>
2022-03-26 10:05:21 +0100

    citanje fajlova scanner motodom

---
+++ src/skener/Ulaz.java
@@ -0,0 +1,18 @@
+package skener;
+
+import java.util.Scanner;
+
+
+public class Ulaz {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Unesite Vas JMBG: ");
+		String jmbg = sc.next();
+		System.out.println("Unesite Vase ime: ");
+		String ime = sc.next();
+		System.out.println("Unesite Vasu platu: ");
+		double plata = sc.nextDouble();
+		System.out.println("JMBG:" + jmbg + " Ime:" + ime + " Plata:" + plata);
+		sc.close();
+	}
+}
